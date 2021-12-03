c52771f124265c3ccd2c1ddfbd92ad2ee78f4f5a
fannyfraiture <ffraiture.fanny@gmail.com>
2021-12-03 16:17:32 +0100

    header start

---
+++ src/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hiking project</title>
+</head>
+<body>
+<header>
+      <nav>
+        <a href="#">
+          <img src="img/logo_hiking.png" img alt="Hiking Project Logo"
+        /></a>
+        <ul class="nav-menu">
+          <li class="button"><a href="#">Services</a></li>
+          <li class="button"><a href="#">About</a></li>
+            <a class="bigbutton" href="#">CONTACT</a>
+        </ul>
+      </nav>
+
+      <h1>Hiking project</h1>
+
+</header>    
+</body>
+</html>
