314994ad610c519939643c1e2e8881874ba6c49d
fannyfraiture <ffraiture.fanny@gmail.com>
2021-12-12 00:37:52 +0100

    update footer

--- src/footer.php
+++ src/footer.php
@@ -8,7 +8,7 @@
             alt="hiking project logo"
           />
         </a>
-        
+        <p class="socialfollow">Follow us</p>
         <ul class="socialmedia">
           <li>
             <a class="social" href="#">
@@ -26,6 +26,26 @@
             <a class="social" href="#">
               <img src="../../img/twitter.png" img alt="twitter"
             /></a>
+          </li>
+        </ul>
+        <p class="contactus">Contact us</p>
+        <ul class="contact">
+          <li class="contact__ctn">
+            <p>
+            <img class="contact__img" src="../../img/phone.png" alt="Phone">
+            +324 74 06 46 19</p>
+          </li>
+
+          <li class="contact__ctn">
+            <p>
+            <img class="contact__img"src="../../img/email.png" alt="Mail">
+            hikingproject@hotmail.com</p>
+          </li>
+
+          <li class="contact__ctn">
+            <p>
+            <img class="contact__img" src="../../img/home.png" alt="Home">
+            Rue de Mulhouse 45<br>4000 Liège</p>
           </li>
         </ul>
       </nav>
