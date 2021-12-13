940085443fa34cd88ef86a167350931296951cd8
fannyfraiture <ffraiture.fanny@gmail.com>
2021-12-13 17:52:10 +0100

    UPDATE SIGNUP

--- src/signup.php
+++ src/signup.php
@@ -2,24 +2,26 @@
     include_once 'header.php';
 ?>
 <section class="create">
-    <h2 class="create__title">Sign Up!</h2>
-
-    <div class="create__ctn">
-
+<div class="create__container">
+    <h2 class="create__container__title">Welcome</h2>
+    <div class="bordure"></div>
+    <img class="create__container__img" src="../../img/login.png" img alt="login"/>
+    <div class="create__container__ctn">
+    <h2 class="create__container__ctn__title">Sign up</h2>
         <form action="includes/signup.inc.php" method="post" >
-            <label for="name">Enter a Username: </label><br>
-            <input type="text" id="name" name="name" placeholder="Username..."><br>
+            <label class="create__container__ctn__label" for="name">Enter a Username: </label><br>
+            <input class="create__container__ctn__input" type="text" id="name" name="name" placeholder="Username..."><br>
             
-            <label for="email">Enter an Email: </label><br>
-            <input type="text" id="email" name="email" placeholder="email@gmail.com..."><br>
+            <label class="create__container__ctn__label" for="email">Enter an Email: </label><br>
+            <input class="create__container__ctn__input" type="text" id="email" name="email" placeholder="email@gmail.com..."><br>
             
-            <label for="pwd">Enter a Password: </label><br>
-            <input type="password" id="pwd" name="pwd"><br>
+            <label class="create__container__ctn__label" for="pwd">Enter a Password: </label><br>
+            <input class="create__container__ctn__input" type="password" id="pwd" name="pwd"><br>
             
-            <label for="repwd">Confirm Password: </label><br>
-            <input type="password" id="repwd" name="repwd"><br>
+            <label class="create__container__ctn__label" for="repwd">Confirm Password: </label><br>
+            <input class="create__container__ctn__input" type="password" id="repwd" name="repwd"><br>
 
-            <button type="submit" value="submit" name="submit">Submit</button>
+            <button class="create__container__ctn__button" type="submit" value="submit" name="submit">Submit</button>
 
         </form>
     </div>
